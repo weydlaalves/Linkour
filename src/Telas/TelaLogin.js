be2a89a27d4ef5c41f892d04fd31ad9b7f50cad6
Weydla Alves <weydlaalves@alu.ufc.br>
2020-09-09 22:57:20 -0300

    Criando as Telas

--- src/Telas/TelaLogin.js
+++ src/Telas/TelaLogin.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import './Pag1.css';
-import Navegador from '../Comum/Navegador';
+import '.TelaLogin.css';
 
 
 class Pag1 extends Component {
     render() {
         return (
-            <div className = 'Pag1'>
+            <div className = 'TelaLogin'>
                 <body>
                     <p> Weydl nao consegue pensar direito</p>
                 </body>
